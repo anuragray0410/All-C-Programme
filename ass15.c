71c5375592489f29f8366e94c11de6966dcee25f
Anurag Ray <tgbarmy28@gmail.com>
2025-09-06 21:39:45 +0530

    Update ass15.c

--- ass15.c
+++ ass15.c
@@ -1,16 +1,21 @@
 #include<stdio.h>
 #include<math.h>
 int main() {
-    float a,b,c,d,n;
+    float a, b, c, d, root1, root2;
     printf("Enter the value of a: ");
-    scanf("%f",&a); 
+    scanf("%f", &a);
     printf("Enter the value of b: ");
-    scanf("%f",&b);
+    scanf("%f", &b);
     printf("Enter the value of c: ");
-    scanf("%f",&c);
-    d=sqrt(b*b-4*a*c);
-    n=(-b+d)/(2*a);
-    printf("the value of root is %.2f\n",n);
-     n=(-b-d)/(2*a);
+    scanf("%f", &c);
+    d = b*b - 4*a*c;
+    if (d < 0) {
+        printf("Roots are imaginary.\n");
+    } else {
+        root1 = (-b + sqrt(d)) / (2*a);
+        root2 = (-b - sqrt(d)) / (2*a);
+        printf("Root 1: %.2f\n", root1);
+        printf("Root 2: %.2f\n", root2);
+    }
     return 0;
 }
