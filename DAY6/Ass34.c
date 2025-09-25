ec666075c5390bb87aee46da00e2e282b8dfb665
Anurag Ray <tgbarmy28@gmail.com>
2025-09-25 23:52:29 +0530

    Update Ass34.c

---
+++ DAY6/Ass34.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+//program for fibonacci series SUING FOR LOOP
+{
+    int n, t1 = 0, t2 = 1, nextTerm, i;
+    printf("Enter the number of terms: ");
+    scanf("%d", &n);
+    printf("Fibonacci Series: ");
+    for (i = 1; i <= n; ++i) {
+        if (i == 1) {
+            printf("%d, ", t1);
+            continue;
+        }
+        if (i == 2) {
+            printf("%d, ", t2);
+            continue;
+        }
+        nextTerm = t1 + t2;
+        t1 = t2;
+        t2 = nextTerm;
+        printf("%d, ", nextTerm);
+    }
+    return 0;
+}
