6a4ee9a254aba59c8c4b064075b175806c43edb8
Anurag Ray <tgbarmy28@gmail.com>
2025-09-25 23:52:03 +0530

    Update Ass33.c

--- DAY6/Ass33.c
+++ DAY6/Ass33.c
@@ -1,14 +1,22 @@
-#include<stdio.h>
-int main()
-//square of the first 10 odd natural numbers and their sum
-{
-    int i,sum=0;
-    for(i=1;i<=19;i+=2)
-    {
-        //
-        printf("The square of %d odd natural number is %d\n",i,i*i);
+#include <stdio.h>
+int main() {
+// Write a c program to print the factorial of a number using for loop.
+int n, i;
+long long fact = 1;
+// long long is used to store large factorials
+    printf("Enter a positive integer: ");
+    scanf("%d", &n);
+
+    // Show error if the user enters a negative integer
+    if (n < 0)
+        printf("Error! Factorial of a negative number doesn't exist.");
+    else {
+        for (i = 1; i <= n; ++i) {
+            fact *= i;              // fact = fact*i;
+            printf("After multiplying by %d, fact = %lld\n", i, fact);
+        }
+        printf("Factorial of %d = %lld", n, fact);
     }
-        sum=sum+(i*i);
-    printf("The sum of the squares of the first 10 odd natural numbers is %d\n",sum);
+
     return 0;
-}
+}                                                                           
